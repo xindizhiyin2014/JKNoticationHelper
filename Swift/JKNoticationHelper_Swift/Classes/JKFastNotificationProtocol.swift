f502aa6b1f594c01172848e72824ec119de87132
xindizhiyin2014 <929097264@qq.com>
2021-09-19 01:13:12 +0800

    增加通知有效的范围相关函数

--- Swift/JKNoticationHelper_Swift/Classes/JKFastNotificationProtocol.swift
+++ Swift/JKNoticationHelper_Swift/Classes/JKFastNotificationProtocol.swift
@@ -21,6 +21,33 @@ public protocol JKFastNotificationProtocol {
     ///   - block: 处理通知的回调
     func jk_observeNotificaions(names:Array<String>,block:@escaping ((_ notification:Notification) -> Void)) ->Void
     
+    /// 在一定范围内监听通知事件
+    /// - Parameters:
+    ///   - moduleName: 模块名
+    ///   - notificationName: 通知名字
+    ///   - block: 通知事件处理
+    func jk_observeNotification(at moduleName:String, notificationName:String, block:@escaping ((_ notification:Notification) -> Void)) ->Void
+    
+    /// 在一定范围内监听通知事件
+    /// - Parameters:
+    ///   - moduleName: 模块名
+    ///   - notificationNames: 通知名字数组
+    ///   - block: 通知事件处理
+    func jk_observeNotifications(at moduleName:String, notificationNames:Array<String>, block:@escaping ((_ notification:Notification) -> Void)) ->Void
+    
+    /// 在一定范围内监听通知事件
+    /// - Parameters:
+    ///   - moduleInstance: 模块实例对象
+    ///   - notificationName: 通知名字
+    ///   - block: 通知事件处理
+    func jk_observeNotification(at moduleInstance:Any, notificationName:String, block:@escaping ((_ notification:Notification) -> Void)) ->Void
+    
+    /// 在一定范围内监听通知事件
+    /// - Parameters:
+    ///   - moduleInstance: 模块实例对象
+    ///   - notificationNames: 通知名字数组
+    ///   - block: 通知事件回调
+    func jk_observeNotifications(at moduleInstance:Any, notificationNames:Array<String>, block:@escaping ((_ notification:Notification) -> Void)) ->Void
     /// 移除通知的监听
     /// - Parameter name: 通知的名字
     func jk_removeObserveNotification(for name:String) ->Void
@@ -35,6 +62,34 @@ public protocol JKFastNotificationProtocol {
     ///   - object: 发送的对象
     ///   - userInfo: 用户信息
     func jk_postNotification(notificationName:String, object:Any?, userInfo:[AnyHashable : Any]?) ->Void
+    
+    /// 在一定范围内发送通知
+    /// - Parameters:
+    ///   - moduleName: 模块名
+    ///   - notificationName: 通知名字
+    func jk_postNotification(at moduleName:String, notificationName:String) -> Void
+    
+    /// 在一定范围内发送通知
+    /// - Parameters:
+    ///   - moduleName: 模块名
+    ///   - notificationName: 通知名字
+    ///   - object: object
+    ///   - userInfo: userInfo
+    func jk_postNotification(at moduleName:String, notificationName:String, object:Any?, userInfo:[AnyHashable : Any]?) -> Void
+    
+    /// 在一定范围内发送通知
+    /// - Parameters:
+    ///   - moduleInstance: 模块实例对象
+    ///   - notificationName: 通知名字
+    func jk_postNotification(at moduleInstance:Any, notificationName:String) -> Void
+    
+    /// 在一定范围内发送通知
+    /// - Parameters:
+    ///   - moduleInstance: 模块实例对象
+    ///   - notificationName: 通知名字
+    ///   - object: object
+    ///   - userInfo: userInfo
+    func jk_postNotification(at moduleInstance:Any, notificationName:String, object:Any?, userInfo:[AnyHashable : Any]?) -> Void
     
     
     var notificationProxyList: [JKFastNotificationProxy] {get set}
@@ -75,6 +130,28 @@ public extension JKFastNotificationProtocol {
         tmpSelf.notificationProxyList += proxyArr
     }
     
+    func jk_observeNotification(at moduleName:String, notificationName:String, block:@escaping ((_ notification:Notification) -> Void)) ->Void {
+        jk_observeNotifications(at: moduleName, notificationNames: [notificationName], block: block)
+    }
+    
+    func jk_observeNotifications(at moduleName:String, notificationNames:Array<String>, block:@escaping ((_ notification:Notification) -> Void)) ->Void {
+        for notificationName in notificationNames {
+            let name = "\(moduleName)##$$&&\(notificationName)"
+            jk_observeNotificaion(name: name, block: block)
+        }
+    }
+    
+    func jk_observeNotification(at moduleInstance:Any, notificationName:String, block:@escaping ((_ notification:Notification) -> Void)) ->Void {
+        jk_observeNotifications(at: moduleInstance, notificationNames: [notificationName], block: block)
+    }
+    
+    func jk_observeNotifications(at moduleInstance:Any, notificationNames:Array<String>, block:@escaping ((_ notification:Notification) -> Void)) ->Void {
+        for notificationName in notificationNames {
+            let name = String.init(format: "%p##$$&&%@", moduleInstance as! CVarArg,notificationName)
+            jk_observeNotificaion(name: name, block: block)
+        }
+    }
+    
      func jk_removeObserveNotification(for name:String) ->Void {
         var tmpSelf = self
         tmpSelf.notificationProxyList.removeAll { $0.notificationName == name}
@@ -86,6 +163,26 @@ public extension JKFastNotificationProtocol {
     
     func jk_postNotification(notificationName:String, object:Any?, userInfo:[AnyHashable : Any]?) ->Void {
         NotificationCenter.default.post(name: Notification.Name.init(notificationName), object: object, userInfo: userInfo)
+    }
+    
+    func jk_postNotification(at moduleName:String, notificationName:String) -> Void {
+        jk_postNotification(at: moduleName, notificationName: notificationName, object: nil,userInfo: nil)
+    }
+    
+    func jk_postNotification(at moduleName:String, notificationName:String, object:Any?, userInfo:[AnyHashable : Any]?) -> Void
+    {
+        let name = "\(moduleName)##$$&&\(notificationName)"
+        jk_postNotification(notificationName: name, object: object, userInfo: userInfo)
+    }
+    
+    func jk_postNotification(at moduleInstance:Any, notificationName:String) -> Void {
+        jk_postNotification(at: moduleInstance, notificationName: notificationName, object: nil, userInfo: nil)
+    }
+    
+    func jk_postNotification(at moduleInstance:Any, notificationName:String, object:Any?, userInfo:[AnyHashable : Any]?) -> Void {
+        let name = String.init(format: "%p##$$&&%@", moduleInstance as! CVarArg,notificationName)
+        jk_postNotification(notificationName: name, object: object, userInfo: userInfo)
+        
     }
 }
 
